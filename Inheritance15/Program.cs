1cc5aa2301eaa5b81a5bdb70c7c394ebd01cc84f
Tim Albert <talbert1209@gmail.com>
2019-03-01 16:01:09 -0600

    Add project files.

---
+++ Inheritance15/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritance15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pay an hourly employee Bob who works for 40 hours
+            // Bob's hourly rate is $27.50
+
+            // Pay a salaried employee Steve who works one week.
+            // Steve's yearly salary is $50,000
+            var employees = new List<Employee>
+            {
+                new HourlyEmployee{Name = "Bob"},
+                new SalariedEmployee{Name = "Steve"}
+            };
+            
+            PayEmployees(employees);
+        }
+
+        private static void PayEmployees(List<Employee> employees)
+        {
+            foreach (var employee in employees)
+            {
+                if (employee.GetType() == typeof(HourlyEmployee))
+                {
+                    employee.PayEmployee(40, 27.50);
+                }
+
+                if (employee.GetType() == typeof(SalariedEmployee))
+                {
+                    employee.PayEmployee(40, 50000);
+                }
+            }
+        }
+    }
+
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+
+        public abstract void PayEmployee(int time, double rate);
+
+    }
+
+    public class HourlyEmployee : Employee
+    {
+        public override void PayEmployee(int hoursWorked, double payRate)
+        {
+            var payCheck = hoursWorked * payRate;
+            Console.WriteLine($"{Name} has been paid {payCheck:C}");
+        }
+    }
+
+    public class SalariedEmployee : Employee
+    {
+        public double BaseSalary { get; set; }
+        public override void PayEmployee(int hoursWorked, double yearlyRate)
+        {
+            var payCheck = yearlyRate / 26;
+            Console.WriteLine($"{Name} has been paid {payCheck:C}");
+        }
+    }
+}
---
+++ Inheritance15/README.txt
@@ -0,0 +1,13 @@
+﻿You have a class called Employee.  You should make it 
+an abstract base class, and make its PayEmployee() abstract, too.
+
+You must create two classes that derive from that Employee
+base class that calculates the pay due the given employee.
+
+Undoubtedly, you will need to extend the properties of the
+derived classes to support the different ways you need to
+pay employees.  Both will need to implement the abstract
+PayEmployee() method.
+
+Print the name and the amount to pay the employee in a 
+Console.WriteLine()
